e24d637927bc6aea1841d70e5a47cf36ab1ae870
Flegontov Ivan <ivanflegont@gmail.com>
2021-07-01 17:25:57 +0300

    Implementation of A-star algorithm for solving simple 15-puzzle game.

---
+++ puzzle/puzzle/PuzzleGame.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using Priority_Queue;
+using System.Text;
+using System.Linq;
+
+namespace puzzle
+{
+    class PuzzleGame
+    {
+        SimplePriorityQueue<SearchNode, double> _nodes;
+        IDictionary<State, double> _previousStates;
+
+        public Stack<State> Solution = null;
+
+        public class SearchNode : IComparable
+        {
+            
+            public State _currentState { get; }
+            public SearchNode _previousNode { get; }
+            public int NumberOfMoves { get; private set; }
+
+            public SearchNode(State state, SearchNode previous)
+            {
+                _currentState = state;
+                if (previous != null)
+                {
+                    NumberOfMoves = previous.NumberOfMoves + 1;
+                    _previousNode = previous;
+                }
+                else
+                {
+                    _previousNode = null;
+                    NumberOfMoves++;
+                }
+            }
+
+            public int CompareTo(Object obj)
+            {
+                if (obj == null) 
+                    return _currentState.Manhattan() + NumberOfMoves;
+
+                SearchNode node = obj as SearchNode;
+                return (_currentState.Manhattan() + NumberOfMoves) - (node._currentState.Manhattan() + node.NumberOfMoves);
+
+            }
+
+            public double Metric()
+            {
+                return _currentState.Manhattan() +  0.25 * NumberOfMoves;
+            }
+        }
+
+        public PuzzleGame(State initial)
+        {
+            _nodes = new SimplePriorityQueue<SearchNode, double>();
+            _previousStates = new Dictionary<State, double>();
+
+            SearchNode x = new SearchNode(initial, null);
+            _nodes.Enqueue(x, x.Metric());
+        }
+
+        private SearchNode Solver()
+        {
+            while (_nodes.Count > 0)
+            {
+                SearchNode x = _nodes.Dequeue();
+                if (x._currentState.IsGoal())
+                {
+                    return x;
+                }
+                
+                foreach (State state in x._currentState.Neighbours())
+                {
+                    if (x._previousNode == null)
+                    {
+                        SearchNode y = new SearchNode(state, x);
+                        _nodes.Enqueue(y, y.Metric());
+                        continue;
+                    }
+
+                    if (!state.Equals(x._previousNode._currentState))
+                    {
+                        if (_previousStates.ContainsKey(state) && state.Metric(x.NumberOfMoves + 1) >= _previousStates[state])
+                            continue;
+                        //if (!_previousStates.ContainsKey(state) || state.Metric(x.NumberOfMoves + 1) < _previousStates[state])
+                        else
+                        {
+                            SearchNode y = new SearchNode(state, x);
+                            _nodes.Enqueue(y, y.Metric());
+                        }
+                    }
+                }
+
+                _previousStates.Add(x._currentState, x.Metric());
+            }
+            return null;
+        }
+
+        public Stack<State> Solve()
+        {
+            if ( Solution == null)
+            {
+                Solution = new Stack<State>();
+                SearchNode node = Solver();
+                while (node != null)
+                {
+                    Solution.Push(node._currentState);
+                    node = node._previousNode;
+                }
+            }
+
+            return Solution;
+        }
+    }
+}
---
+++ puzzle/puzzle/State.cs
@@ -0,0 +1,215 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace puzzle
+{
+    class State
+    {
+        // lenght of game table
+        public int N { get;  } 
+        public int CurEmpty { get; private set; }
+        public int[,] Array { get; }
+
+        public State(int n, int curEmpty, int[,] array)
+        {
+            N = n;
+            CurEmpty = curEmpty;
+            Array = array;
+        }
+
+        public State(int n, int curEmpty, int[] array)
+        {
+            if (array.Length != n * n)
+                throw new Exception("Wrong array");
+
+            N = n;
+            CurEmpty = curEmpty;
+            Array = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                {
+                    Array[i, j] = array[i * N + j];
+                }
+            
+        }
+
+        public static State RandomState(int n) 
+        {
+            IEnumerable<int> numbers = Enumerable.Range(0, n*n);
+            Random rnd = new Random();
+            int[] permutation = numbers.OrderBy(x => rnd.Next()).ToArray();
+
+            int curEmpty =  System.Array.FindIndex(permutation, EqualsZero);
+
+            return new State(n, curEmpty, permutation);
+        }
+
+        //returns new State with two swapped elements in array
+        // [i, j] <—> [m, n]
+        public State Swap(int i, int j, int m,  int n)
+        {
+
+            if (i < 0 || j < 0 || m < 0 || n < 0 || i >= N || j >= N || m>=N || n >= N)
+                throw new Exception("Wrong Indices");
+
+            int[,] array = Array.Clone() as int[,];
+
+            int curEmpty = CurEmpty;
+
+            if (array[i, j] == 0)
+                curEmpty = m * N + n;
+            else if (array[m, n] == 0)
+                curEmpty = i * N + j;
+
+            int tmp = array[i, j];
+            array[i, j] = array[m, n];
+            array[m, n] = tmp;
+
+            return new State(N, curEmpty, array);
+
+        }
+     
+        public IEnumerable<State> Neighbours()
+        {
+            List<State> neighbours = new List<State>();
+            int j = CurEmpty % N;
+            int i = (CurEmpty - j) / N;
+            if (i > 0)
+                neighbours.Add(this.Swap(i, j, i - 1, j));
+            if (i < N - 1)
+                neighbours.Add(this.Swap(i, j, i + 1, j));
+            if (j > 0)
+                neighbours.Add(this.Swap(i, j, i, j - 1));
+            if (j < N - 1)
+                neighbours.Add(this.Swap(i, j, i, j + 1));
+            return neighbours;
+        }
+
+        public int Manhattan()
+        {
+            int dist = 0;
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                {
+                    if( Array[i, j] == 0)
+                        continue;
+                    int new_j = (Array[i, j] - 1) % N;
+                    int new_i = (Array[i, j] - 1 - new_j ) / N;
+                    dist += Math.Abs(new_i - i) + Math.Abs(new_j - j);
+                }
+            return dist;
+        }
+
+
+
+        /*
+         * State has solution if and only if one of the cases holdes true
+         * 
+         *  1) if N = 2k + 1:
+         *      sgn(per) = 1;
+         *      
+         *  2.1) if N = 2k and whitespace (0) is at even row ( row = 0, 1, ..., 2k - 1):
+         *      sgn(per) = -1;
+         *      
+         *  2.2) if N = 2k and whitespace (0) is at odd row:
+         *      sgn(per) = 1.
+         *      
+         *   Where per is a permutation without whitespace represented by 0. Thus per \in S_{N*N - 1}
+         */
+        public bool IsSolvable()
+        {
+            if (N % 2 == 1)
+            {
+                if (IsEven())
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                int j = CurEmpty % N;
+                int i = (CurEmpty - j) / N;
+                if (i % N == 0 && !IsEven())
+                    return true;
+                else if (i % N == 1 && IsEven())
+                    return true;
+                else 
+                    return false;
+            }
+        }
+
+        public bool IsEven()
+        {
+
+            int[] permutation = Array.Cast<int>().Where(val => val != 0).ToArray().Clone() as int[];
+
+            int swap_counter = 0;
+
+            for (int i = 0; i < N * N - 2;)
+            {
+                if (i == permutation[i] - 1)
+                {
+                    i++;
+                    continue;
+                }
+                else
+                {
+                    int k = permutation[i];
+                    permutation[i] = permutation[k - 1];
+                    permutation[k - 1] = k;
+                    swap_counter++;
+                }
+            }
+            return (swap_counter % 2) == 0;
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder str = new StringBuilder();
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                    str.Append($" {Array[i, j]} |");
+
+                str.Append("\n");
+            }
+            str.Append("\n");
+            return str.ToString();
+        }
+
+        public bool IsGoal()
+        {
+            return Manhattan() == 0;
+        }
+
+        public bool Equals(State y)
+        {
+            if (y == null)
+                return false;
+            if (CurEmpty != y.CurEmpty || N != y.N)
+                return false;
+            return Array.Cast<int>().SequenceEqual(y.Array.Cast<int>());
+        }
+
+        public int Metric(int moves)
+        {
+            return Manhattan() + moves;
+        }
+
+        private static bool EqualsZero(int x)
+        {
+            if (x == 0)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
